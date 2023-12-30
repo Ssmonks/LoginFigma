b5f242ef96e8fc728ec58f59585be32c18f5c9f3
Ivan H <ivan2herrerra@gmail.com>
2023-12-30 10:47:42 -0400

    fixed page scroll

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { useRef, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import {
+	Dimensions,
 	Image,
 	ImageBackground,
 	Pressable,
@@ -28,6 +29,7 @@ import FacebbokLogo from './assets/images/facebookLogo.svg';
 import PointsOfPassword from './assets/images/pointsOfPassword.svg';
 import { emailCheck, passwordCheck } from './helpers/validationFunctions';
 import OverlayOnProcess from './components/overlays/overlayOnProcess';
+import Constants from 'expo-constants';
 
 const fontsToUse = {
 	'Poppins-Medium': require('./assets/fonts/poppins/Poppins-Regular.ttf'),
@@ -50,246 +52,242 @@ export default function App() {
 
 	if (fontsLoaded) {
 		return (
-			<SafeAreaProvider>
-				<View style={styles.container}>
-					<ImageBackground
-						resizeMode={'stretch'}
-						source={require('./assets/images/Background.png')}
-						style={styles.backgroundContentImage}
-					>
-						<View style={styles.headerContainer}>
-							<ImageBackground
-								resizeMode={'stretch'}
-								source={require('./assets/images/Objects.png')}
-								style={styles.backgroundHeaderImage}
+			<ScrollView>
+				<ImageBackground
+					resizeMode={'stretch'}
+					source={require('./assets/images/Background.png')}
+					style={styles.backgroundContentImage}
+				>
+					<View style={styles.headerContainer}>
+						<ImageBackground
+							resizeMode={'stretch'}
+							source={require('./assets/images/Objects.png')}
+							style={styles.backgroundHeaderImage}
+						>
+							<Image source={require('./assets/images/Illustration.png')} style={styles.puppetContainer}></Image>
+						</ImageBackground>
+					</View>
+					<LinearGradient colors={['#B379DF20', '#CC585408']} style={styles.contentContainer}>
+						<Text style={styles.title}>Get Started Free</Text>
+						<Text style={styles.subTitle}>Free Forever. No Credit Card Needed</Text>
+						<View style={styles.formContainer}>
+							<Text style={styles.textInputTitle}>Email Address</Text>
+							<LinearGradient
+								colors={blackLinearGradientColors}
+								start={{ x: 0, y: 0.5 }}
+								end={{ x: 1, y: 0.5 }}
+								style={styles.textInputLinearGradient}
 							>
-								<Image source={require('./assets/images/Illustration.png')} style={styles.puppetContainer}></Image>
-							</ImageBackground>
-						</View>
-						<LinearGradient colors={['#B379DF20', '#CC585408']} style={styles.contentContainer}>
-							<ScrollView>
-								<Text style={styles.title}>Get Started Free</Text>
-								<Text style={styles.subTitle}>Free Forever. No Credit Card Needed</Text>
-								<View style={styles.formContainer}>
-									<Text style={styles.textInputTitle}>Email Address</Text>
-									<LinearGradient
-										colors={blackLinearGradientColors}
-										start={{ x: 0, y: 0.5 }}
-										end={{ x: 1, y: 0.5 }}
-										style={styles.textInputLinearGradient}
-									>
-										<TextInput
-											mode="flat"
-											style={styles.textInputStyle}
-											activeUnderlineColor="transparent"
-											cursorColor="#A4A4A4"
-											left={<TextInput.Icon icon="email-outline" color="#A4A4A4" rippleColor="transparent" />}
-											placeholder="yourname@gmail.com"
-											placeholderTextColor="#A4A4A4"
-											theme={{
-												colors: { text: 'white' },
-												fonts: { regular: { fontFamily: 'Poppins-Medium' } },
-											}}
-											onChangeText={(input) => setEmail(input)}
-										/>
-									</LinearGradient>
-									<Text style={styles.textInputTitle}>Your Name</Text>
-									<LinearGradient
-										colors={blackLinearGradientColors}
-										start={{ x: 0, y: 0.5 }}
-										end={{ x: 1, y: 0.5 }}
-										style={styles.textInputLinearGradient}
-									>
-										<TextInput
-											mode="flat"
-											style={styles.textInputStyle}
-											activeUnderlineColor="transparent"
-											cursorColor="#A4A4A4"
-											left={<TextInput.Icon icon="account-outline" color="#A4A4A4" rippleColor="transparent" />}
-											placeholder="@yourname"
-											placeholderTextColor="#A4A4A4"
-											theme={{ colors: { text: 'white' }, fonts: { regular: { fontFamily: 'Poppins-Medium' } } }}
-											onChangeText={(input) => setUserName(input)}
-										/>
-									</LinearGradient>
-									<Text style={styles.textInputTitle}>Password</Text>
-									<Pressable
-										onPress={() => {
-											passwordInputRef.current.focus();
-										}}
-									>
-										<LinearGradient
-											colors={blackLinearGradientColors}
-											start={{ x: 0, y: 0.5 }}
-											end={{ x: 1, y: 0.5 }}
-											style={styles.textInputLinearGradient}
-										>
-											<View style={styles.textInputPasswordStyle}>
-												<View style={styles.textInputPasswordIconContainer}>
-													<Pressable onPress={() => setPasswordVisible(!passwordVisible)}>
-														{passwordVisible ? <Key /> : <KeyOff />}
-													</Pressable>
-													<TextInputNative
-														ref={passwordInputRef}
-														style={{
-															fontFamily: 'Poppins-Medium',
-															color: 'white',
-															fontSize: 16,
-															marginLeft: writingPassword || password.length >= 1 ? 10 : 0,
-															width: password.length >= 1 ? '55%' : 1,
-														}}
-														cursorColor="#A4A4A4"
-														secureTextEntry={passwordVisible}
-														onChangeText={(input) => {
-															setPassword(input);
-														}}
-														onFocus={() => {
-															setWrittingPasword(true);
-														}}
-														onEndEditing={() => setWrittingPasword(false)}
-													/>
-													{password.length < 1 ? (
-														<View style={{ marginLeft: writingPassword && password.length < 1 ? 0 : 10 }}>
-															<PointsOfPassword />
-														</View>
-													) : (
-														<></>
-													)}
+								<TextInput
+									mode="flat"
+									style={styles.textInputStyle}
+									activeUnderlineColor="transparent"
+									cursorColor="#A4A4A4"
+									left={<TextInput.Icon icon="email-outline" color="#A4A4A4" rippleColor="transparent" />}
+									placeholder="yourname@gmail.com"
+									placeholderTextColor="#A4A4A4"
+									theme={{
+										colors: { text: 'white' },
+										fonts: { regular: { fontFamily: 'Poppins-Medium' } },
+									}}
+									onChangeText={(input) => setEmail(input)}
+								/>
+							</LinearGradient>
+							<Text style={styles.textInputTitle}>Your Name</Text>
+							<LinearGradient
+								colors={blackLinearGradientColors}
+								start={{ x: 0, y: 0.5 }}
+								end={{ x: 1, y: 0.5 }}
+								style={styles.textInputLinearGradient}
+							>
+								<TextInput
+									mode="flat"
+									style={styles.textInputStyle}
+									activeUnderlineColor="transparent"
+									cursorColor="#A4A4A4"
+									left={<TextInput.Icon icon="account-outline" color="#A4A4A4" rippleColor="transparent" />}
+									placeholder="@yourname"
+									placeholderTextColor="#A4A4A4"
+									theme={{ colors: { text: 'white' }, fonts: { regular: { fontFamily: 'Poppins-Medium' } } }}
+									onChangeText={(input) => setUserName(input)}
+								/>
+							</LinearGradient>
+							<Text style={styles.textInputTitle}>Password</Text>
+							<Pressable
+								onPress={() => {
+									passwordInputRef.current.focus();
+								}}
+							>
+								<LinearGradient
+									colors={blackLinearGradientColors}
+									start={{ x: 0, y: 0.5 }}
+									end={{ x: 1, y: 0.5 }}
+									style={styles.textInputLinearGradient}
+								>
+									<View style={styles.textInputPasswordStyle}>
+										<View style={styles.textInputPasswordIconContainer}>
+											<Pressable onPress={() => setPasswordVisible(!passwordVisible)}>
+												{passwordVisible ? <Key /> : <KeyOff />}
+											</Pressable>
+											<TextInputNative
+												ref={passwordInputRef}
+												style={{
+													fontFamily: 'Poppins-Medium',
+													color: 'white',
+													fontSize: 16,
+													marginLeft: writingPassword || password.length >= 1 ? 10 : 0,
+													width: password.length >= 1 ? '55%' : 1,
+												}}
+												cursorColor="#A4A4A4"
+												secureTextEntry={passwordVisible}
+												onChangeText={(input) => {
+													setPassword(input);
+												}}
+												onFocus={() => {
+													setWrittingPasword(true);
+												}}
+												onEndEditing={() => setWrittingPasword(false)}
+											/>
+											{password.length < 1 ? (
+												<View style={{ marginLeft: writingPassword && password.length < 1 ? 0 : 10 }}>
+													<PointsOfPassword />
 												</View>
-												{password.length > 1 ? (
-													<View style={styles.passwordStrongCheckerContainer}>
-														{password.length < 5 ? (
-															<>
-																<View style={styles.passwordStrongCheckerIndicators}>
-																	<Weak />
-																</View>
-																<View style={styles.passwordStrongCheckerIndicators}>
-																	<NotAllowed />
-																</View>
-																<View>
-																	<NotAllowed />
-																</View>
-																<Text style={styles.paswordStrongCheckerWeakText}>Weak</Text>
-															</>
-														) : password.length < 8 ? (
-															<>
-																<View style={styles.passwordStrongCheckerIndicators}>
-																	<Medium />
-																</View>
-																<View style={styles.passwordStrongCheckerIndicators}>
-																	<Medium />
-																</View>
-																<View>
-																	<NotAllowed />
-																</View>
-																<Text style={styles.passwordStrongCheckerMediumText}>Medium</Text>
-															</>
-														) : (
-															<>
-																<View style={styles.passwordStrongCheckerIndicators}>
-																	<Allowed />
-																</View>
-																<View style={styles.passwordStrongCheckerIndicators}>
-																	<Allowed />
-																</View>
-																<View>
-																	<Allowed />
-																</View>
-																<Text style={styles.passwordStrongCheckerStrongText}>Strong</Text>
-															</>
-														)}
-													</View>
+											) : (
+												<></>
+											)}
+										</View>
+										{password.length > 1 ? (
+											<View style={styles.passwordStrongCheckerContainer}>
+												{password.length < 5 ? (
+													<>
+														<View style={styles.passwordStrongCheckerIndicators}>
+															<Weak />
+														</View>
+														<View style={styles.passwordStrongCheckerIndicators}>
+															<NotAllowed />
+														</View>
+														<View>
+															<NotAllowed />
+														</View>
+														<Text style={styles.paswordStrongCheckerWeakText}>Weak</Text>
+													</>
+												) : password.length < 8 ? (
+													<>
+														<View style={styles.passwordStrongCheckerIndicators}>
+															<Medium />
+														</View>
+														<View style={styles.passwordStrongCheckerIndicators}>
+															<Medium />
+														</View>
+														<View>
+															<NotAllowed />
+														</View>
+														<Text style={styles.passwordStrongCheckerMediumText}>Medium</Text>
+													</>
 												) : (
-													<></>
+													<>
+														<View style={styles.passwordStrongCheckerIndicators}>
+															<Allowed />
+														</View>
+														<View style={styles.passwordStrongCheckerIndicators}>
+															<Allowed />
+														</View>
+														<View>
+															<Allowed />
+														</View>
+														<Text style={styles.passwordStrongCheckerStrongText}>Strong</Text>
+													</>
 												)}
 											</View>
-										</LinearGradient>
-									</Pressable>
-									<TouchableOpacity
-										onPress={() => {
-											//Validate Data to Create User
-											if (emailCheck(email, setMessage)) {
-												if (userName.length > 1)
-													if (passwordCheck(password, setMessage)) {
-														//Activate Overlay to Send User Message
-														setTitle('Logrado!');
-														setMessage('Usuario creado exitosamente');
-														setIsOverlayVisible(true);
-													} else {
-														//Activate Overlay to Send User Message
-														setTitle('Error');
-														setIsOverlayVisible(true);
-													}
-												else {
-													//Activate Overlay to Send User Message
-													setTitle('Error');
-													setMessage('Debe escribir un nombre de usuario');
-													setIsOverlayVisible(true);
-												}
+										) : (
+											<></>
+										)}
+									</View>
+								</LinearGradient>
+							</Pressable>
+							<TouchableOpacity
+								onPress={() => {
+									//Validate Data to Create User
+									if (emailCheck(email, setMessage)) {
+										if (userName.length > 1)
+											if (passwordCheck(password, setMessage)) {
+												//Activate Overlay to Send User Message
+												setTitle('Logrado!');
+												setMessage('Usuario creado exitosamente');
+												setIsOverlayVisible(true);
 											} else {
 												//Activate Overlay to Send User Message
 												setTitle('Error');
 												setIsOverlayVisible(true);
 											}
-										}}
+										else {
+											//Activate Overlay to Send User Message
+											setTitle('Error');
+											setMessage('Debe escribir un nombre de usuario');
+											setIsOverlayVisible(true);
+										}
+									} else {
+										//Activate Overlay to Send User Message
+										setTitle('Error');
+										setIsOverlayVisible(true);
+									}
+								}}
+							>
+								<LinearGradient
+									colors={['#9C3FE4', '#C65647']}
+									start={{ x: 0, y: 0.5 }}
+									end={{ x: 1, y: 0.5 }}
+									style={styles.signUpButtonLinearGradient}
+								>
+									<Text style={styles.signUpButtonTextStyle}>Sign Up</Text>
+								</LinearGradient>
+							</TouchableOpacity>
+							<OrWith />
+							<View style={styles.socialButtonsContainer}>
+								<TouchableOpacity>
+									<LinearGradient
+										colors={blackLinearGradientColors}
+										start={{ x: 0, y: 0.5 }}
+										end={{ x: 1, y: 0.5 }}
+										style={styles.socialButtonsStyle}
 									>
-										<LinearGradient
-											colors={['#9C3FE4', '#C65647']}
-											start={{ x: 0, y: 0.5 }}
-											end={{ x: 1, y: 0.5 }}
-											style={styles.signUpButtonLinearGradient}
-										>
-											<Text style={styles.signUpButtonTextStyle}>Sign Up</Text>
-										</LinearGradient>
-									</TouchableOpacity>
-									<OrWith />
-									<View style={styles.socialButtonsContainer}>
-										<TouchableOpacity>
-											<LinearGradient
-												colors={blackLinearGradientColors}
-												start={{ x: 0, y: 0.5 }}
-												end={{ x: 1, y: 0.5 }}
-												style={styles.socialButtonsStyle}
-											>
-												<GoogleLogo />
-											</LinearGradient>
-										</TouchableOpacity>
-										<TouchableOpacity>
-											<LinearGradient
-												colors={blackLinearGradientColors}
-												start={{ x: 0, y: 0.5 }}
-												end={{ x: 1, y: 0.5 }}
-												style={styles.socialButtonsStyle}
-											>
-												<AppleLogo />
-											</LinearGradient>
-										</TouchableOpacity>
-										<TouchableOpacity>
-											<LinearGradient
-												colors={blackLinearGradientColors}
-												start={{ x: 0, y: 0.5 }}
-												end={{ x: 1, y: 0.5 }}
-												style={styles.socialButtonsStyle}
-											>
-												<View style={{ marginTop: 5 }}>
-													<FacebbokLogo />
-												</View>
-											</LinearGradient>
-										</TouchableOpacity>
-									</View>
-								</View>
-							</ScrollView>
-						</LinearGradient>
-					</ImageBackground>
-					<StatusBar style="light" />
-					<OverlayOnProcess
-						isVisible={isOverlayVisible}
-						setIsVisible={setIsOverlayVisible}
-						title={title}
-						subTitle={message}
-					/>
-				</View>
-			</SafeAreaProvider>
+										<GoogleLogo />
+									</LinearGradient>
+								</TouchableOpacity>
+								<TouchableOpacity>
+									<LinearGradient
+										colors={blackLinearGradientColors}
+										start={{ x: 0, y: 0.5 }}
+										end={{ x: 1, y: 0.5 }}
+										style={styles.socialButtonsStyle}
+									>
+										<AppleLogo />
+									</LinearGradient>
+								</TouchableOpacity>
+								<TouchableOpacity>
+									<LinearGradient
+										colors={blackLinearGradientColors}
+										start={{ x: 0, y: 0.5 }}
+										end={{ x: 1, y: 0.5 }}
+										style={styles.socialButtonsStyle}
+									>
+										<View style={{ marginTop: 5 }}>
+											<FacebbokLogo />
+										</View>
+									</LinearGradient>
+								</TouchableOpacity>
+							</View>
+						</View>
+					</LinearGradient>
+				</ImageBackground>
+				<StatusBar backgroundColor="#151316" style="light" />
+				<OverlayOnProcess
+					isVisible={isOverlayVisible}
+					setIsVisible={setIsOverlayVisible}
+					title={title}
+					subTitle={message}
+				/>
+			</ScrollView>
 		);
 	}
 }
@@ -300,22 +298,25 @@ const styles = StyleSheet.create({
 		backgroundColor: '#151316',
 	},
 	backgroundContentImage: {
-		flex: 1,
+		marginTop: Constants.statusBarHeight,
 		flexDirection: 'column',
-		position: 'absolute',
-		width: '100%',
-		height: '100%',
+		width: Dimensions.get('window').width,
+		height: Dimensions.get('window').height,
+		backgroundColor: '#151316',
 	},
 	headerContainer: {
-		height: 375,
+		height: '46%',
 		width: '100%',
 	},
 	backgroundHeaderImage: {
-		width: '100%',
-		height: '100%',
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
 	},
 	puppetContainer: {
-		marginTop: '-10%',
+		marginTop: '-20%',
 		alignSelf: 'flex-end',
 		resizeMode: 'contain',
 		width: '65%',
@@ -323,7 +324,7 @@ const styles = StyleSheet.create({
 	contentContainer: {
 		flex: 1,
 		flexDirection: 'column',
-		marginTop: -130,
+		marginTop: '-40%',
 		width: '100%',
 		borderTopRightRadius: 40,
 		borderTopLeftRadius: 40,
@@ -376,7 +377,6 @@ const styles = StyleSheet.create({
 		fontFamily: 'Poppins-Medium',
 	},
 	textInputPasswordStyle: {
-		flex: 1,
 		flexDirection: 'row',
 		paddingHorizontal: 20,
 		height: 45,
